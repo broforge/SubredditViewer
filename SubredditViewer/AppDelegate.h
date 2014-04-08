ce46c485ff0d4549f99022049ae58cc692746bde
BrotoMan <BrotoMan@Roberts-MacBook-Pro.local>
2014-04-07 20:57:23 -0700

    Initial Commit

---
+++ SubredditViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SubredditViewer
+//
+//  Created by BrotoMan on 4/7/14.
+//  Copyright (c) 2014 BrotoMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SubredditViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SubredditViewer
+//
+//  Created by BrotoMan on 4/7/14.
+//  Copyright (c) 2014 BrotoMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
